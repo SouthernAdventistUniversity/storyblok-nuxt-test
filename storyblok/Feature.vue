358f9f33eaa7de971400c6a1b81b3c6fd5065370
Isaac Braun <isaacbraun2000@gmail.com>
2024-08-13 10:48:11 -0400

    feat: added optional image

--- storyblok/Feature.vue
+++ storyblok/Feature.vue
@@ -3,12 +3,20 @@
     v-editable="blok"
     class="w-full p-12 bg-[#f7f6fd] rounded-[5px] text-center"
   >
-    <h3 class="text-2xl text-[#1d243d] font-bold">
-      {{ blok.name }}
-    </h3>
+    <img
+      v-if="blok.image"
+      :src="`${blok.image.filename}/m/`"
+      :alt="blok.image.alt"
+      class="w-full h-48 xl:h-72 object-cover"
+    />
+    <div class="px-12 py-6">
+      <h3 class="text-2xl text-[#1d243d] font-bold">
+        {{ blok.name }}
+      </h3>
+    </div>
   </div>
 </template>
- 
+
 <script setup>
-defineProps({ blok: Object })
+defineProps({ blok: Object });
 </script>
